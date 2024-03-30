4e7277ffd11c7d42e31989451f5277b3e947de1d
unknown <kajtekkups@gmail.com>
2024-03-30 18:45:28 +0100

    uwagi na przyszlosc

--- ESP32_Telemetry/src/main.cpp
+++ ESP32_Telemetry/src/main.cpp
@@ -14,6 +14,7 @@ void setup() {
   establish_mqtt_connection();
 
   init_ADC();
+  //jezeli nie wykryje sd to zapali lampke i nie bedzie blokowac programu
 
   micro_sd_file.begin();
 
@@ -22,8 +23,12 @@ void setup() {
 
 void loop() {
 
+  // wyeliminowac blokowanie braku polaczenia z internetem
   if (!MQTT_client.connected()) MQTT_reconnect(); // check if MQTT client is connected
   //TODO: zrobic podobnie dla wifi
+  //TODO: dodać obsługę diod:
+    //brak polaczenia z SD
+    //brak polaczenia z serwerem MQTT
   
   Collect_data(); //zbierz pomiary 
   Send_save_data(); //wyslij pomiary na serwer
